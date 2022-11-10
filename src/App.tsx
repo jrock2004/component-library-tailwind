f5db7808790f731ed2b500e802bc8567ccfc5df3
John Costanzo <jrock2004@gmail.com>
2022-11-10 16:50:37 -0500

    chore: setting up storybook

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ReactElement } from "react";
+import { ReactElement } from 'react';
 
 const App = (): ReactElement => {
   return <div>App</div>;
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,7 @@
+import { ReactElement } from 'react';
+
+export const Button = (): ReactElement => {
+  return (
+    <button className="bg-transparent text-4xl">This is a temp Button</button>
+  );
+};
