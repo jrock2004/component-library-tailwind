a362bdf7305d4f7fe906f7b3cf57f98e18eaf7e4
John Costanzo <jrock2004@gmail.com>
2022-11-10 16:25:45 -0500

    chore: adding tailwind support

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path")
+
+module.exports = {
+  content: [path.join(__dirname, "./src/**/*.(js|jsx|ts|tsx)")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
